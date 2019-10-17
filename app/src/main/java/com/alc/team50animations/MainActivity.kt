762dc1a31387ce294eb44d9cda81e52b4480ff99
Sithe Ncube <me.sithe.ncube@gmail.com>
2019-10-17 15:57:38 +0200

    Created Initial Project File :sunflower:
    Create initial project file using animation course project as base;
    Added Buttons for scenarios to be implemented;
    Created first scenario;

---
+++ app/src/main/java/com/alc/team50animations/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.alc.team50animations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.animation.AnimatorInflater
+import android.content.Intent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun rotateAnimation(view: View) {
+
+        val rotateAnimator = AnimatorInflater.loadAnimator(this, R.animator.rotate)
+        rotateAnimator?.apply {
+            setTarget(targetImage)
+            start()
+        }
+    }
+
+    fun scaleAnimation(view: View) {
+
+        val scaleAnimator = AnimatorInflater.loadAnimator(this, R.animator.scale)
+        scaleAnimator?.apply {
+            setTarget(targetImage)
+            start()
+        }
+    }
+
+    fun translateAnimation(view: View) {
+
+        val translateAnimator = AnimatorInflater.loadAnimator(this, R.animator.translate)
+        translateAnimator.apply {
+            setTarget(targetImage)
+            start()
+        }
+    }
+
+    fun fadeAnimation(view: View) {
+
+        val fadeAnimator = AnimatorInflater.loadAnimator(this, R.animator.alpha)
+        fadeAnimator.apply{
+            setTarget(targetImage)
+            start()
+        }
+    }
+
+    fun startScenario1(view: View) {
+        val intent = Intent(this, AnimConstraintActivity::class.java)
+        startActivity(intent)
+    }
+}
+
